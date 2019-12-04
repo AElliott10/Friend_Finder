b48e2be135dcc73b5e76b3d5864e565e817225be
Chingford <chingford162@outlook.com>
2019-12-04 18:38:17 -0500

    final version without readme

--- app/routing/apiRouting.js
+++ app/routing/apiRouting.js
@@ -12,44 +12,40 @@ module.exports = function (app) {
     var bestMatch = {
       name: "",
       photo: "",
-      friendsDifference: Infinity
+      friendsDifference: 1000
     }
 
     //Here is were we take the survey POST(results) and parse it.
     var userData = req.body;
     var userScores = userData.scores;
 
-    console.log(userScores);
+    // console.log("=====");
+    // console.log("scores: ", userScores);
+    // console.log("=====");
+    // console.log("userData: ", userData);
+    // console.log("=====");
 
     var totalDifference = 0;
 
     //Nested for loop first to go through all the friends in the database
     for (var i = 0; i < friends.length; i++) {
-      console.log(friends[i]);
+      console.log("1st for loop "); console.log(friends[i]);
       totalDifference = 0;
       //Then loop through the scores of each friend
       for (let j = 0; j < friends[i].scores[j]; j++) {
 
-        totalDifference += Math.abs(parseInt(underScores[j]) - parseInt(friends[i].scores[j]));
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
       }
-
+      console.log("total diff: ", totalDifference);
       if (totalDifference <= bestMatch.friendsDifference) {
         bestMatch.name = friends[i].name;
         bestMatch.photo = friends[i].photo;
         bestMatch.friendsDifference = totalDifference;
       }
     }
-
+    console.log("userData: ", userData);
     friends.push(userData);
-
+    console.log("friends array: ", friends);
     res.json(bestMatch);
-  })
-
-
-  // Capture Survey information - takes in JSON input
-
-
+  });
 }
-// Displays all possible friends
-
-
